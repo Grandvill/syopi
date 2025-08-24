75f3ffd3abc5dbe5ef17b02ecfeb4c290dd87ec3
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-08-24 12:37:17 +0700

    Add : seeder category

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,14 +15,48 @@ class CategorySeeder extends Seeder
         $categories = [
             [
                 'name' => 'Elektronik',
-                'icon' => 'category/elektronik.webp',
+                'icon' => 'category/Elektronik.webp',
                 'childs' => ['Microwave', 'TV'],
             ],
             [
-                'name' => 'Elektronik',
-                'icon' => 'category/elektronik.webp',
-                'childs' => ['Microwave', 'TV'],
+                'name' => 'Fashion Pria',
+                'icon' => 'category/Fashion-pria.webp',
+                'childs' => ['Kemeja', 'Jas'],
+            ],
+            [
+                'name' => 'Fashion Wanita',
+                'icon' => 'category/Fashion-wanita.webp',
+                'childs' => ['Dress', 'Jas'],
+            ],
+            [
+                'name' => 'Handphone',
+                'icon' => 'category/Handphone.webp',
+                'childs' => ['Handphone', 'Anti Gores'],
+            ],
+            [
+                'name' => 'Komputer & Laptop',
+                'icon' => 'category/Komputer-Laptop.webp',
+                'childs' => ['Laptop', 'Keyboard'],
+            ],
+            [
+                'name' => 'Makanan & Minuman',
+                'icon' => 'category/Makanan-Minuman.webp',
+                'childs' => ['Makanan', 'Minuman'],
             ]
         ];
+
+        foreach ($categories as $categoryPayload) {
+            $category = \App\Models\Category::create([
+                'slug' => \Str::slug($categoryPayload['name']),
+                'name' => $categoryPayload['name'],
+                'icon' => $categoryPayload['icon'],
+            ]);
+            foreach ($categoryPayload['childs'] as $child) {
+                $category->childs()->create([
+                    'slug' => \Str::slug($child),
+                    'name' => $child,
+                ]);
+            }
+        }
     }
 }

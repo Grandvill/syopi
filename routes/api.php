076090790064054decc9c3f3b0d1d73ce8c62dbd
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-08-26 20:20:43 +0700

    Add : login and register API with google

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\AddressController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+Route::post('/google-auth', [AuthenticationController::class, 'authGoogle']);
 Route::post('/register', [AuthenticationController::class, 'register']);
 Route::post('/resend-otp', [AuthenticationController::class, 'resendOtp']);
 Route::post('/check-otp-register', [AuthenticationController::class, 'verifyOtp']);

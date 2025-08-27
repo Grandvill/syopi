9edae6df956bb0facd9752c4936e792d367f8989
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-08-27 14:22:29 +0700

    Update : review model

--- app/Models/Product/Review.php
+++ app/Models/Product/Review.php
@@ -26,4 +26,45 @@ class Review extends Model
         'attachments' => 'array',
         'show_username' => 'boolean',
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(\App\Models\Product\Product::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function getAttachmentsAttribute($value)
+    {
+        // add storage path to each attachment
+        $attachments = json_decode($value);
+        $attachments = array_map(function ($attachment) {
+            return asset('storage/' . $attachment);
+        }, $attachments);
+
+        return $attachments;
+    }
+
+    // Get API response
+    public function getApiResponseAttribute()
+    {
+        return [
+            'star_seller' => $this->star_seller,
+            'variations' => $this->variations,
+            'description' => $this->description,
+            'attachments' => $this->attachments,
+            'show_username' => $this->show_username,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'user_photo' => $this->show_username ? $this->user->photo_url : null,
+            'user_name' => $this->show_username ? $this->user->name : substr($this->user->name, 0, 1) . str_repeat('*', strlen($this->user->name) - 2) . substr($this->user->name, -1),
+        ];
+    }
+
+    public function setAttachmentsAttribute($value)
+    {
+        $this->attributes['attachments'] = json_encode($value);
+    }
 }

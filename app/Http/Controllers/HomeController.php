59afed0a8594879158dd2ea5877b217aa14edd64
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-08-28 18:10:27 +0700

    Add : filter get product API

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
 
     public function getProduct()
     {
-        $products = \App\Models\Product\Product::orderBy('id', 'desc');
+        $products = \App\Models\Product\Product::query();
 
         if (!is_null(request()->category)) {
             $category = \App\Models\Category::where('slug', request()->category)->firstOrFail();
@@ -37,6 +37,27 @@ class HomeController extends Controller
 
         if (!is_null(request()->search)) {
             $products->where('name', 'LIKE', '%' . request()->search . '%');
+        }
+
+        if (!is_null(request()->minimum_price)) {
+            $products->whereRaw('IF(price_sale > 0, price_sale, price) >= ?', request()->minimum_price);
+        }
+
+        if (!is_null(request()->maximum_price)) {
+            $products->whereRaw('IF(price_sale > 0, price_sale, price) <= ?', request()->maximum_price);
+        }
+
+        if (!is_null(request()->sorting_price)) {
+            $type = request()->sorting_price == 'asc' ? 'ASC' : 'DESC';
+            $products->orderByRaw('IF(price_sale > 0, price_sale, price) ' . $type);
+        } else {
+            $products->orderBy('id', 'desc');
+        }
+
+        if (!is_null(request()->categories) && is_array(request()->categories)) {
+            $products->whereHas('category', function($subQuery){
+                $subQuery->whereIn('slug', request()->categories);
+            });
         }
 
         $products = $products->paginate(request()->per_page ?? 10);

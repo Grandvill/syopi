7944ad18c43525885d580ca595073e99c3cdce1e
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-09-06 18:50:45 +0700

    Add : handle callback midtrans

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/testing', function () {
-    $response = Http::asForm()->withHeaders([
-        'key' => config('services.rajaongkir.api_key'),
-        'Accept' => 'application/json',
-    ])->post(config('services.rajaongkir.base_url') . '/calculate/domestic-cost', [
-        'origin' => 128,
-        'destination' => 17,
-        'weight' => 500,
-        'courier' => 'jne',
-    ]);
+// Route::get('/testing', function () {
+//     $response = Http::asForm()->withHeaders([
+//         'key' => config('services.rajaongkir.api_key'),
+//         'Accept' => 'application/json',
+//     ])->post(config('services.rajaongkir.base_url') . '/calculate/domestic-cost', [
+//         'origin' => 128,
+//         'destination' => 17,
+//         'weight' => 500,
+//         'courier' => 'jne',
+//     ]);
 
-    $result = collect($response->object());
+//     $result = collect($response->object());
 
-    dd($result);
-});
+//     dd($result);
+// });
+
+Route::post('/midtrans/callback', [\App\Http\Controllers\MidtransController::class, 'callback']);

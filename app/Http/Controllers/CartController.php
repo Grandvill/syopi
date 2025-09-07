5c1e8a9f63371280908c8a96f43f6a4fcbacb1dc
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-09-07 14:42:04 +0700

    Add : toggle coin API

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -440,5 +440,20 @@ class CartController extends Controller
         return ResponseFormatter::success($order->api_response_detail);
     }
 
+    public function toggleCoin()
+    {
+        $cart = $this->getOrCreateCart();
 
+        $coin = 0;
+        if (request()->use == 1) {
+            $balance = auth()->user()->balance;
+            $maxCoin = $cart->items->sum('total') * 0.1;
+            $coin = $balance > $maxCoin ? $maxCoin : $balance;
+        }
+
+        $cart->pay_with_coin = $coin;
+        $cart->save();
+
+        return $this->getCart();
+    }
 }

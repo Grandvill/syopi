d9f2aa3725946b69118baeb2557391f7e352f8fc
Zahidan Ardhiansyah <zahidan23@gmail.com>
2025-09-07 15:54:44 +0700

    Update : email template

--- resources/views/mails/register-otp.blade.php
+++ resources/views/mails/register-otp.blade.php
@@ -1,3 +1,9 @@
+@extends('mails.layout')
+
+@section('content')
 Hi {{ $user->name }},<br>
 
 Ini adalah kode OTP Register Anda: {{ $user->otp_register }}
+
+@endsection
+
--- resources/views/mails/reset-password-otp.blade.php
+++ resources/views/mails/reset-password-otp.blade.php
@@ -1,5 +1,12 @@
+@extends('mails.layout')
+
+@section('content')
 Hi {{ $user->name }},<br>
 
 Anda akan melakukan reset password, masukkan OTP berikut pada aplikasi: {{ $otp }}.<br>
 
 Abaikan jika Anda tidak melakukan reset password.
+
+@endsection
+
+
